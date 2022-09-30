b3a640ab5c141f53ab49a31b97ab38a5f397a31e
Mazen Mohamed <85415062+Mazen72@users.noreply.github.com>
2022-10-01 00:22:46 +0200

    Update map_page.py

--- map_page.py
+++ map_page.py
@@ -6,7 +6,7 @@ import functions
 def get_layout(df):
     # a text to inform the user that hovering shows more details
     info_text = html.Div('Hover over a country to see more info..', 
-                            style=dict(fontSize='1.6vh', fontWeight='', color='black',
+                            style=dict(fontSize='1.9vh', fontWeight='', color='black',
                                        textAlign="left", width='100%', paddingLeft='2rem'))
 
     # map figure part
